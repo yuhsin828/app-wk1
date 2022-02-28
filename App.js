455521de69d5934fc64047679c893ee0e35aeeb4
yuhsin828 <71396690+yuhsin828@users.noreply.github.com>
2022-02-28 17:13:03 +0800

    third

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.user}>數位三甲</Text>
+      <Text style={styles.user}>數位</Text>
       <Text style={styles.user}>110819018 蔡鈺昕</Text>
       <StatusBar style="auto" />
     </View>
